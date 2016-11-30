5fc05bdfbb0fb0a3899fc6502e5e617aea00457b
Nick Tomlin <nick.tomlin@gmail.com>
2016-11-29 18:27:31 -0600

    chore: Add module level specs

--- spec/kefir_spec.rb
+++ spec/kefir_spec.rb
@@ -108,5 +108,21 @@ RSpec.describe Kefir do
         Kefir.config('')
       end.to raise_exception(Kefir::MissingNamespaceError)
     end
+
+    it 'returns a fully functioning config object that can read config' do
+      allow(File).to receive(:read).and_yield("user: bob\napi_key: secret-value")
+
+      config = Kefir.config('test')
+
+      expect(config.get('user')).to eq('bob')
+    end
+
+    it 'returns a fully functioning config object that can write config' do
+      allow(File).to receive(:read).and_yield("user: bob\napi_key: secret-value")
+      expect(File).to receive(:write)
+
+      config = Kefir.config('test')
+      config.store
+    end
   end
 end
