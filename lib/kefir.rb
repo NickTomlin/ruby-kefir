45f01f58e3be0be38f92c62444fe695a59c529eb
Nick Tomlin <nick.tomlin@gmail.com>
2016-11-30 09:05:16 -0600

    feat: Allow overriding of config_name and cwd

--- lib/kefir.rb
+++ lib/kefir.rb
@@ -8,12 +8,26 @@ require 'kefir/file_store'
 module Kefir
   class MissingNamespaceError < StandardError; end
 
+  DEFAULT_OPTIONS = {
+    config_name: 'config.yml'
+  }.freeze
+
   def self.config(namespace, options = {})
     raise MissingNamespaceError, 'You must supply a namespace for your configuration files' if namespace.nil? || namespace.empty?
 
-    config_file_path = EnvPaths.get(namespace).config
-    store = FileStore.new(config_file_path)
+    parsed_options = parse_options(namespace, options)
+    store = FileStore.new(parsed_options[:config_file_path])
 
     Config.new(store, options)
+  end
+
+  private_class_method
+
+  def self.parse_options(namespace, options)
+    parsed = DEFAULT_OPTIONS.merge(options)
+    cwd = parsed.fetch(:cwd, EnvPaths.get(namespace).config)
+    parsed[:config_file_path] = File.expand_path(parsed[:config_name], cwd)
+
+    parsed
   end
 end
--- spec/kefir_spec.rb
+++ spec/kefir_spec.rb
@@ -27,5 +27,29 @@ RSpec.describe Kefir do
       config = Kefir.config('test')
       config.persist
     end
+
+    context 'options' do
+      it 'provides defaults' do
+        expect(File).to receive(:read).with(/config\.yml$/).and_yield("user: bob\napi_key: secret-value")
+        config = Kefir.config('test')
+        config.get(:foo)
+      end
+
+      it 'allows overriding current directory' do
+        custom_cwd = File.expand_path('custom/path', '/')
+        expect(File).to receive(:read).with(File.join(custom_cwd, 'config.yml')).and_yield("user: bob\napi_key: secret-value")
+
+        config = Kefir.config('test', cwd: custom_cwd)
+        config.get(:foo)
+      end
+
+      it 'allows overriding the :config_name' do
+        custom_cwd = File.expand_path('custom/path', '/')
+        expect(File).to receive(:read).with(File.join(custom_cwd, 'custom_config_name.yml')).and_yield("user: bob\napi_key: secret-value")
+
+        config = Kefir.config('test', cwd: custom_cwd, config_name: 'custom_config_name.yml')
+        config.get(:foo)
+      end
+    end
   end
 end
